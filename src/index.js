83582762d27d3670c8ba76b18dc52ac7f81da5ab
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-04-12 11:45:33 +0300

    gallery and cards

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import "./styles/index.scss";
 
 document.getElementById("year").innerHTML = new Date().getFullYear();
+
+const filterSelectEl = document.getElementById('filter');
+const itemsEl = document.getElementById('items');
+
+filterSelectEl.onchange = function() {
+	console.log(this.value);
+  const items = itemsEl.getElementsByClassName('item');
+  for (let i=0; i<items.length; i++) {
+if (items[i].classList.contains(this.value)) {
+items[i].style.display = 'block';
+    } else {
+    items[i].style.display = 'none';
+    }
+  }
+};
