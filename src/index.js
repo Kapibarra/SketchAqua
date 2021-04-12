f2c9c08011786e231345e51e1635b558f5c216ce
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-04-12 15:27:10 +0300

    scroll

--- src/index.js
+++ src/index.js
@@ -47,3 +47,30 @@ if (document.querySelector(".gallery__main")) {
       }
     };   
 }
+
+/* scroll */
+
+const scrollToTopButton = document.getElementById("js-top");
+const scrollFunc = () => {
+const y = window.scrollY;
+if (y > 200) {
+scrollToTopButton.className = "top-link show";
+} else {
+scrollToTopButton.className = "top-link hide";
+}
+};
+
+window.addEventListener("scroll", scrollFunc);
+
+const scrollToTop = () => {
+const c = document.documentElement.scrollTop || document.body.scrollTop;
+if (c > 0) {
+window.requestAnimationFrame(scrollToTop);
+window.scrollTo(0, c - c / 18);
+}
+};
+
+scrollToTopButton.onclick = function (e) {
+e.preventDefault();
+scrollToTop();
+};
