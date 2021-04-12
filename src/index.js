b03addd5899bfad4c4b56e2c7178d01918fa8a25
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-04-12 13:56:29 +0300

    burger

--- src/index.js
+++ src/index.js
@@ -1,18 +1,49 @@
 import "./styles/index.scss";
-
-document.getElementById("year").innerHTML = new Date().getFullYear();
-
-const filterSelectEl = document.getElementById('filter');
-const itemsEl = document.getElementById('items');
-
-filterSelectEl.onchange = function() {
-	console.log(this.value);
-  const items = itemsEl.getElementsByClassName('item');
-  for (let i=0; i<items.length; i++) {
-if (items[i].classList.contains(this.value)) {
-items[i].style.display = 'block';
+/* BURGER*/
+(function () {
+    const hamburger = {
+      navToggle: document.querySelector('.nav-toggle'),
+      nav: document.querySelector('nav'),
+  
+      doToggle: function (e) {
+        e.preventDefault();
+        this.navToggle.classList.toggle('expanded');
+        this.nav.classList.toggle('expanded');
+      },
+    };
+    hamburger.navToggle.addEventListener('click', function (e) { hamburger.doToggle(e); });
+    // hamburger.nav.addEventListener('click', function(e) { hamburger.doToggle(e); });
+  }());
+  
+  function burgerButtonToggle() {
+    const burgerButton = document.querySelector(".nav-toggle")
+    const yPos = window.scrollY;
+    if (yPos > 10) {
+      burgerButton.classList.add("unvisible")
+      burgerButton.classList.remove("visible");
     } else {
-    items[i].style.display = 'none';
+      burgerButton.classList.remove("unvisible");
+      burgerButton.classList.add("visible");
     }
   }
-};
+
+  /* YEAR copyright */
+  window.addEventListener("scroll", burgerButtonToggle);
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+/* Gallery sort */
+if (document.querySelector(".gallery__main")) {
+    const filterSelectEl = document.getElementById('filter');
+    const itemsEl = document.getElementById('items');
+    filterSelectEl.onchange = function() {
+        console.log(this.value);
+      const items = itemsEl.getElementsByClassName('item');
+      for (let i=0; i<items.length; i++) {
+    if (items[i].classList.contains(this.value)) {
+    items[i].style.display = 'block';
+        } else {
+        items[i].style.display = 'none';
+        }
+      }
+    };   
+}
