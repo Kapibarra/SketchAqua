9b176c05e902d410e75befa4688f02fc85222ca7
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-04-13 23:55:51 +0300

    new positions in catalog

--- src/catalog.js
+++ src/catalog.js
@@ -1,193 +1,269 @@
 const CATALOG = [
-    {
-        "id": 1,
-        "description": "Размер А5 140*198, акварельная бумага низкой плотности",
-        "category": "sketch",
-        "photo": "../img/cards/card1.png",
-        "price": 350+' '+'RUB'
-    },
-    {
-        "id": 2,
-        "description": "Размер А5 140*198, акварельная бумага низкой плотности",
-        "category": "sketch",
-        "photo": "../img/cards/card2.png",
-        "price": 350+' '+'RUB'
-    },
-    {
-        "id": 3,
-        "description": "Размер А5 140*198, акварельная бумага низкой плотности",
-        "category": "sketch",
-        "photo": "../img/cards/card3.png",
-        "price": 350+' '+'RUB'
-    },
-    {
-        "id": 4,
-        "description": "Размер А5 140*198, акварельная бумага низкой плотности",
-        "category": "sketch",
-        "photo": "../img/cards/card4.png",
-        "price": 350+' '+'RUB'
-    },
-    {
-        "id": 5,
-        "description": "Размер А5 140*198, акварельная бумага низкой плотности",
-        "category": "sketch",
-        "photo": "../img/cards/card5.png",
-        "price": 350+' '+'RUB'
-    },
-    {
-        "id": 6,
-        "description": "Размер А5 140*198, акварельная бумага низкой плотности",
-        "category": "sketch",
-        "photo": "../img/cards/card6.png",
-        "price": 350+' '+'RUB'
-    },
-    {
-        "id": 7,
-        "description": "Размер А5 140*198, акварельная бумага низкой плотности",
-        "category": "sketch",
-        "photo": "../img/cards/card7.png",
-        "price": 350+' '+'RUB'
-    },
-    {
-        "id": 8,
-        "description": "Размер А5 140*198, акварельная бумага низкой плотности",
-        "category": "sketch",
-        "photo": "../img/cards/card8.png",
-        "price": 350+' '+'RUB'
-    },
-    {
-        "id": 9,
-        "description": "Размер А5 140*198, акварельная бумага низкой плотности",
-        "category": "sketch",
-        "photo": "../img/cards/card9.png",
-        "price": 350+' '+'RUB'
-    },
-    {
-        "id": 10,
-        "description": "Размер А5 140*198, акварельная бумага низкой плотности",
-        "category": "sketch",
-        "photo": "../img/cards/card10.png",
-        "price": 350+' '+'RUB'
-    },
-    {
-        "id": 11,
-        "description": "Размер А5 140*198, акварельная бумага низкой плотности",
-        "category": "sketch",
-        "photo": "../img/cards/card11.png",
-        "price": 350+' '+'RUB'
-    },
-    {
-        "id": 12,
-        "description": "Размер А5 140*198, акварельная бумага низкой плотности",
-        "category": "sketch",
-        "photo": "../img/cards/card12.png",
-        "price": 350+' '+'RUB'
-    },
-    {
-        "id": 13,
-        "description": "Размер А5 140*198, акварельная бумага низкой плотности",
-        "category": "sketch",
-        "photo": "../img/cards/card13.png",
-        "price": 350+' '+'RUB'
-    },
-    {
-        "id": 14,
-        "description": "Размер А5 140*198, акварельная бумага низкой плотности",
-        "category": "sketch",
-        "photo": "../img/cards/card14.png",
-        "price": 350+' '+'RUB'
-    },
-    {
-        "id": 15,
-        "description": "Размер А5 140*198, акварельная бумага низкой плотности",
-        "category": "sketch",
-        "photo": "../img/cards/card15.png",
-        "price": 350+' '+'RUB'
-    },
-    {
-        "id": 16,
-        "description": "Размер А5 140*198, акварельная бумага низкой плотности",
-        "category": "sketch",
-        "photo": "../img/cards/card16.png",
-        "price": 350+' '+'RUB'
-    },
-    {
-        "id": 17,
-        "description": "Размер А5 140*198, акварельная бумага низкой плотности",
-        "category": "sketch",
-        "photo": "../img/cards/card17.png",
-        "price": 350+' '+'RUB'
-    },
-    {
-        "id": 18,
-        "description": "Размер А5 140*198, акварельная бумага низкой плотности",
-        "category": "sketch",
-        "photo": "../img/cards/card18.png",
-        "price": 350+' '+'RUB'
-    },
-    {
-        "id": 19,
-        "description": "Размер А5 140*198, акварельная бумага низкой плотности",
-        "category": "sketch",
-        "photo": "../img/cards/card19.png",
-        "price": 350+' '+'RUB'
-    },
-    {
-        "id": 20,
-        "description": "Размер А5 140*198, акварельная бумага низкой плотности",
-        "category": "sketch",
-        "photo": "../img/cards/card20.png",
-        "price": 350+' '+'RUB'
-    },
-    {
-        "id": 21,
-        "description": "image1",
-        "category": "sketch",
-        "photo": "../img/cards/card21.png",
-        "price": 350+' '+'RUB'
-    },
-    {
-        "id": 22,
-        "description": "image1",
-        "category": "sketch",
-        "photo": "../img/cards/card22.png",
-        "price": 350+' '+'RUB'
-    },
-    {
-        "id": 23,
-        "description": "image1",
-        "category": "sketch",
-        "photo": "../img/cards/card23.png",
-        "price": 350+' '+'RUB'
-    },
-    {
-        "id": 24,
-        "description": "image1",
-        "category": "sketch",
-        "photo": "../img/cards/card24.png",
-        "price": 350+' '+'RUB'
-    },
-    {
-        "id": 25,
-        "description": "image1",
-        "category": "sketch",
-        "photo": "../img/cards/card25.png",
-        "price": 350+' '+'RUB'
-    },
-    {
-        "id": 26,
-        "description": "image1",
-        "category": "sketch",
-        "photo": "../img/cards/card26.png",
-        "price": 350+' '+'RUB'
-    },
-    {
-        "id": 27,
-        "description": "image1",
-        "category": "sketch",
-        "photo": "../img/cards/card27.png",
-        "price": 350+' '+'RUB'
-    }
-    
-]
-export default CATALOG
+  {
+    id: 1,
+    description: "Размер А5 140*198, акварельная бумага низкой плотности",
+    category: "sketch",
+    photo: "../img/cards/card1.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 2,
+    description: "Размер А5 140*198, акварельная бумага низкой плотности",
+    category: "sketch",
+    photo: "../img/cards/card2.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 3,
+    description: "Размер А5 140*198, акварельная бумага низкой плотности",
+    category: "sketch",
+    photo: "../img/cards/card3.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 4,
+    description: "Размер А5 140*198, акварельная бумага низкой плотности",
+    category: "sketch",
+    photo: "../img/cards/card4.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 5,
+    description: "Размер А5 140*198, акварельная бумага низкой плотности",
+    category: "sketch",
+    photo: "../img/cards/card5.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 6,
+    description: "Размер А5 140*198, акварельная бумага низкой плотности",
+    category: "sketch",
+    photo: "../img/cards/card6.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 7,
+    description: "Размер А5 140*198, акварельная бумага низкой плотности",
+    category: "sketch",
+    photo: "../img/cards/card7.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 8,
+    description: "Размер А5 140*198, акварельная бумага низкой плотности",
+    category: "sketch",
+    photo: "../img/cards/card8.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 9,
+    description: "Размер А5 140*198, акварельная бумага низкой плотности",
+    category: "sketch",
+    photo: "../img/cards/card9.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 10,
+    description: "Размер А5 140*198, акварельная бумага низкой плотности",
+    category: "sketch",
+    photo: "../img/cards/card10.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 11,
+    description: "Размер А5 140*198, акварельная бумага низкой плотности",
+    category: "sketch",
+    photo: "../img/cards/card11.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 12,
+    description: "Размер А5 140*198, акварельная бумага низкой плотности",
+    category: "sketch",
+    photo: "../img/cards/card12.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 13,
+    description: "Размер А5 140*198, акварельная бумага низкой плотности",
+    category: "sketch",
+    photo: "../img/cards/card13.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 14,
+    description: "Размер А5 140*198, акварельная бумага низкой плотности",
+    category: "sketch",
+    photo: "../img/cards/card14.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 15,
+    description: "Размер А5 140*198, акварельная бумага низкой плотности",
+    category: "sketch",
+    photo: "../img/cards/card15.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 16,
+    description: "Размер А5 140*198, акварельная бумага низкой плотности",
+    category: "sketch",
+    photo: "../img/cards/card16.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 17,
+    description: "Размер А5 140*198, акварельная бумага низкой плотности",
+    category: "sketch",
+    photo: "../img/cards/card17.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 18,
+    description: "Размер А5 140*198, акварельная бумага низкой плотности",
+    category: "sketch",
+    photo: "../img/cards/card18.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 19,
+    description: "Размер А5 140*198, акварельная бумага низкой плотности",
+    category: "sketch",
+    photo: "../img/cards/card19.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 20,
+    description: "Размер А5 140*198, акварельная бумага низкой плотности",
+    category: "sketch",
+    photo: "../img/cards/card20.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 21,
+    description: "image1",
+    category: "sketch",
+    photo: "../img/cards/card21.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 22,
+    description: "image1",
+    category: "sketch",
+    photo: "../img/cards/card22.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 23,
+    description: "image1",
+    category: "sketch",
+    photo: "../img/cards/card23.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 24,
+    description: "image1",
+    category: "sketch",
+    photo: "../img/cards/card24.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 25,
+    description: "image1",
+    category: "sketch",
+    photo: "../img/cards/card25.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 26,
+    description: "image1",
+    category: "sketch",
+    photo: "../img/cards/card26.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 27,
+    description: "image1",
+    category: "sketch",
+    photo: "../img/cards/card27.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 28,
+    description: "image1",
+    category: "sketch",
+    photo: "../img/cards/card28.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 29,
+    description: "image1",
+    category: "sketch",
+    photo: "../img/cards/card29.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 30,
+    description: "image1",
+    category: "sketch",
+    photo: "../img/cards/card30.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 31,
+    description: "image1",
+    category: "sketch",
+    photo: "../img/cards/card31.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 32,
+    description: "image1",
+    category: "sketch",
+    photo: "../img/cards/card32.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 33,
+    description: "image1",
+    category: "sketch",
+    photo: "../img/cards/card33.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 34,
+    description: "image1",
+    category: "sketch",
+    photo: "../img/cards/card34.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 35,
+    description: "image1",
+    category: "sketch",
+    photo: "../img/cards/card35.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 36,
+    description: "image1",
+    category: "sketch",
+    photo: "../img/cards/card36.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 37,
+    description: "image1",
+    category: "sketch",
+    photo: "../img/cards/card37.png",
+    price: 350 + " " + "RUB",
+  },
+  {
+    id: 38,
+    description: "image1",
+    category: "sketch",
+    photo: "../img/cards/card38.png",
+    price: 350 + " " + "RUB",
+  },
+];
+export default CATALOG;
