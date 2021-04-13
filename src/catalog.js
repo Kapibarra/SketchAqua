3b63535635c1dc6f690b6506eabe172490efc58a
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-04-13 14:20:38 +0300

    fix catalog

--- src/catalog.js
+++ src/catalog.js
@@ -3,28 +3,28 @@ const CATALOG = [
         "id": 1,
         "name": "image1",
         "category": "sketch",
-        "photo": "../img/card.png",
+        "photo": "../img/card1.png",
         "price": 2999
     },
     {
         "id": 2,
         "name": "image2",
         "category": "sketch",
-        "photo": "../img/card.png",
+        "photo": "../img/card1.png",
         "price": 299
     },
     {
         "id": 3,
         "name": "image3",
         "category": "sketch",
-        "photo": "../img/card.png",
+        "photo": "../img/card1.png",
         "price": 29
     },
     {
         "id": 4,
         "name": "image4",
         "category": "sketch",
-        "photo": "../img/card.png",
+        "photo": "../img/card1.png",
         "price": 2
     }
 ]
--- src/js/Catalog.js
+++ src/js/Catalog.js
@@ -2,11 +2,34 @@ import CATALOG from '../catalog.js';
 
 class Products {
     render() {
-        CATALOG.forEach((element) => {
-            console.log(element)
+        let htmlCatalog = ''
+        CATALOG.forEach(({id,name,price,category,photo}) => {
+            console.log(id,name,price,category,photo)
+           htmlCatalog += `
+           <div class="main__card item ${category} ${id}">
+           <div class="main__card-picture">
+               <img src="${photo}">
+           </div>
+           <p class="main__card-price">ЦЕНА: ${price}</p>
+           <p class="main__card-price">Название: ${name}</p>
+           <a class="btn"><button class="button">BUTTON</button></a>
+       </div>
+           `
         })
+document.getElementById('items').innerHTML = htmlCatalog
     }
 }
 
-const productsPage = new Products();
+ const productsPage = new Products();
 productsPage.render()
+
+
+
+/* <div class="main__card item aquarelle">
+<div class="main__card-picture">
+    <img src="../img/card1.png">
+</div>
+<p class="main__card-price">ЦЕНА: 9999RUB</p>
+<a class="btn"><button class="button">BUTTON</button></a>
+</div>
+ */
